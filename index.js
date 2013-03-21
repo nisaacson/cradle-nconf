64bf28b3051b6d40efd3d6810f1e1a4d78ee2600
Noah Isaacson <noah@nisaacson.com>
2013-03-20 21:46:22 -0400

    switched to passing config as a parameter

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var config = require('nconf')
 var cradle = require('cradle')
-var couch = config.get('couch')
-var host = config.get('couch:host')
-var fullHost = couch.protocol + '://' + couch.host
-var port = config.get('couch:port')
-var database = config.get('couch:database')
-var opts = {
-  cache: false,
-  raw: false
-}
-var username = config.get('couch:username')
-var password = config.get('couch:password')
-if (username) {
-  opts.auth = {
-    username: username,
-    password: password
+module.exports = function (config) {
+  var couch = config.get('couch')
+  var host = config.get('couch:host')
+  var fullHost = couch.protocol + '://' + couch.host
+  var port = config.get('couch:port')
+  var database = config.get('couch:database')
+  var opts = {
+    cache: false,
+    raw: false
   }
+  var username = config.get('couch:username')
+  var password = config.get('couch:password')
+  if (username) {
+    opts.auth = {
+      username: username,
+      password: password
+    }
+  }
+  var c = new(cradle.Connection)(fullHost, port, opts)
+  module.exports = c.database(database)
 }
-var c = new(cradle.Connection)(fullHost, port, opts)
-module.exports = c.database(database)
